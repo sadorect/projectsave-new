46d671f489fa82bd9f379f225375c593df869fec
Tosin Omojola <sadorect@gmail..com>
2024-12-20 03:51:21 +0000

    modified lesson_progress table

---
+++ database/migrations/2024_12_20_034903_modify_last_accessed_at_in_lesson_progress_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('lesson_progress', function (Blueprint $table) {
+            $table->dropColumn('last_accessed_at');
+            $table->timestamp('completed_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('lesson_progress', function (Blueprint $table) {
+            $table->dropColumn('completed_at');
+            $table->timestamp('last_accessed_at');
+        });
+    }
+};
